4dccb4d292e913a282e38bcdabe514b9882a930e
mithesh24fps <mitheshgawande2115@gmail.com>
2021-10-27 16:11:31 +0530

    First commit

---
+++ dev2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class dev2{
+public static void main(String args[]) throws Exception
+{
+System.out.println("My name is Mithesh Gawande and I am from TEIT. My roll no. is 65");
+}
+}
---
+++ dev3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class dev3{
+public static void main(String[] args) throws Exception
+{
+int n=Integer.parseInt(args[0]);
+for(int i=1;i<=10;i++)
+{
+System.out.println(n+" * "+i+" = "+n*i); 
+}
+}
+}
